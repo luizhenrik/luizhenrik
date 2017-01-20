23bf93548dfc7c71e92184625af46ae5ada8c1e7
Luiz Henrik <Luiz Henrik>
2017-01-20 01:10:32 -0200

    estrutura do banner

--- public/index.php
+++ public/index.php
@@ -3,6 +3,7 @@
 <head>
 	<meta charset="UTF-8">
     <link rel="stylesheet" href="assets/css/style.css">
+    <link href="https://fonts.googleapis.com/css?family=Roboto+Condensed:300,400,400i,700" rel="stylesheet">
 	<title>Site Blog FC</title>
 </head>
 <body>
@@ -16,7 +17,7 @@
 
         <main class="grid grid--container">
             <div class="row row--no-gutters">
-                <section class="col--xs-12 col--sm-12 col--md-8 col--lg-8 col--xl-8">
+                <section class="col col--no-gutters">
                     <article class="row row--no-gutters">
                         <header class="col col--no-gutters">
                             section-header
@@ -24,10 +25,18 @@
                         </header>
 
                         <main class="col col--no-gutters">
-                            section-main
-                            <?php include 'home/taboos.php'; ?>
-                            <?php include 'home/tips.php'; ?>
-                            <?php include 'home/news.php'; ?>
+                            <div class="row row--no-gutters">
+                                <div class="col--xs-12 col--sm-12 col--md-8 col--lg-8 col--xl-8">
+                                    section-main
+                                    <?php include 'home/taboos.php'; ?>
+                                    <?php include 'home/tips.php'; ?>
+                                    <?php include 'home/news.php'; ?>
+                                </div>
+                                <aside class="col--xs-12 col--sm-12 col--md-4 col--lg-4 col--xl-4">
+                                    <h2>aside</h2>
+                                    <?php include 'sidebar.php'; ?>
+                                </aside>
+                            </div>
                         </main>
 
                         <footer class="col col--no-gutters">
@@ -36,10 +45,6 @@
                         </footer>
                     </article>
                 </section>
-                <aside class="col--xs-12 col--sm-12 col--md-4 col--lg-4 col--xl-4">
-                    <h2>aside</h2>
-                    <?php include 'sidebar.php'; ?>
-                </aside>
             </div>
         </main>
 
